d90312602bf84ce3ad4c51d0c4a2e94e0c095b56
dianlujitao <dianlujitao@lineageos.org>
2018-02-20 22:47:08 +0800

    grus: Build LiveDisplay HAL
    Change-Id: I1a1f630bd8c4670b3fe9c16d2493019f0aa56a95

--- device.mk
+++ device.mk
@@ -171,6 +171,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_grus
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.mokee.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/media,$(TARGET_COPY_OUT_VENDOR)/etc)
