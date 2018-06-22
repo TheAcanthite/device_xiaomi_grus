9632ed50f5ed22222a47f9378ef002b2bdbb6707
Dan Willemsen <dwillemsen@google.com>
2018-06-21 22:00:35 -0700

    grus: Mark as having overridden commands
    To allow turning these into errors on other devices, and incrementally
    fix these issue, set BUILD_BROKEN_DUP_RULES.
    
    Bug: 77611511
    Test: check out/soong.log
    Change-Id: I3b8f29f0f9e2ad445e72c65e7688b109817dcff3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,10 @@
 DEVICE_PATH := device/xiaomi/grus
 
 TARGET_BOARD_PLATFORM := sdm710
+BUILD_BROKEN_DUP_RULES := true
 
+# Architecture
+>>>>>>> 71b0aab... grus: Mark as having overridden commands
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
