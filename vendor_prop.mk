eb2fb530f9a450c913d1299476c19fdccf6c631d
TheScarastic <warabhishek@gmail.com>
2018-10-13 09:24:27 -0400

    grus: Force enable VoLTE
    For some reason VoLTE wasn't working on some carriers even with
    updated carrierconfig overlays, so add it for now.
    
    Change-Id: I18b5356e1f5b8c8623e569a1fc526394e7e9e247

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -126,4 +126,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac \
     persist.vendor.qcom.bluetooth.enable.splita2dp=true \
     persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac \
-    ro.bluetooth.a2dp_offload.supported=true
+    ro.bluetooth.a2dp_offload.supported=true \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1
