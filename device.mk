7172cd9bd71e617c32882cbd4b715bf23dd6d888
shoudil <stephenli@codeaurora.org>
2019-01-09 17:04:11 +0800

    grus: Add framework detect modules to PRODUCT_PACKAGES
    Add JNI and libqti_vndfwk_detect module name to product_packages
    to specify module to be installed to device. Here explicitly
    specify both the core and vendor variant in PRODUCT_PACKAGES.
    
    CRs-Fixed: 2372775
    
    Change-Id: Ic64f5ac0cfbbe9e105a6b48b5bc7687c8d23d7e8

--- device.mk
+++ device.mk
@@ -264,6 +264,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # QCOM
+PRODUCT_PACKAGES += \
+    libqti_vndfwk_detect.vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
