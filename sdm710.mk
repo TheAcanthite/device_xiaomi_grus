147b767acc34a76b76897aac362a46b3b52e909d
Mahantesh Sanakall <msanak@codeaurora.org>
2019-03-21 15:35:04 +0530

    Set PRODUCT_BRAND to qti
    Switch to non-default PRODUCT_BRAND setting to allow CTS result
    reporting through Android mechanism.
    
    Change-Id: I0e23ff57e48e577823f5669cec3a36ee836e992b

--- sdm710.mk
+++ sdm710.mk
@@ -15,7 +15,7 @@ $(call inherit-product, device/qcom/common/common64.mk)
 
 PRODUCT_NAME := sdm710
 PRODUCT_DEVICE := sdm710
-PRODUCT_BRAND := Android
+PRODUCT_BRAND := qti
 PRODUCT_MODEL := SDM710 for arm64
 
 #Initial bringup flags
