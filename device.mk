61cf7075b2fb65c30f293ef223012b7a1ea3dd6a
subdragonzj <zj502407679@gmail.com>
2019-11-21 22:27:30 +0800

    grus: Define screen density
    Change-Id: If0c50d9f529eee36884c56d666f4883966f99630

--- device.mk
+++ device.mk
@@ -5,6 +5,10 @@ $(call inherit-product-if-exists, vendor/xiaomi/grus/grus-vendor.mk)
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # Override heap growth limit due to high display density on device
 PRODUCT_PROPERTY_OVERRIDES += \
   dalvik.vm.heapgrowthlimit=256m
