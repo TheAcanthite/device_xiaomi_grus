661ffcb863e33f4d4ff8093667e36fe366f2ba54
subdragonzj <zj502407679@gmail.com>
2019-11-24 20:56:33 +0800

    grus: Import qcom permission config
    Change-Id: I791b7bf403f3c43a7c7f269be841200d61571da6

--- device.mk
+++ device.mk
@@ -200,6 +200,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# QCOM
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+
 # WLAN chipset
 WLAN_CHIPSET := qca_cld3
 
