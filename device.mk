64c1f0fad07f5a05e429d2b7fdfd97109765a073
subdragonzj <zj502407679@gmail.com>
2019-11-24 16:17:10 +0800

    grus: Build bluetooth package
    Change-Id: I02e9c3abc2b8c84030ec0b818ed85d77e7ab2524

--- device.mk
+++ device.mk
@@ -79,6 +79,12 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.a2dp@1.0-impl \
+    bt-mac-generator \
+    libbthost_if
+
 TARGET_USES_NQ_NFC := true
 
 # WLAN chipset
