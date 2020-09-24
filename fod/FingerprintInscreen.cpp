sYYLG-TEMP/android_device_xiaomi_grus

grus: Implement in-screen fingerprint HAL
Change-Id: I8fd985bf4eab1747351f9e717cc33fa6d5ab1517

fod/FingerprintInscreen.cpp
@@ -1,0 +1,139 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "FingerprintInscreenService"
+
+#include "FingerprintInscreen.h"
+
+#include <android-base/logging.h>
+#include <fstream>
+
+#define COMMAND_NIT 10
+#define PARAM_NIT_FOD 3
+#define PARAM_NIT_NONE 0
+
+#define DISPPARAM_PATH "/sys/class/drm/card0-DSI-1/disp_param"
+#define DISPPARAM_FOD_BACKLIGHT_HBM "0xD007FF"
+#define DISPPARAM_FOD_BACKLIGHT_RESET "0xD01000"
+
+#define FOD_STATUS_PATH "/sys/devices/virtual/touch/tp_dev/fod_status"
+#define FOD_STATUS_ON 1
+#define FOD_STATUS_OFF 0
+
+#define FOD_SENSOR_X 455
+#define FOD_SENSOR_Y 1920
+#define FOD_SENSOR_SIZE 173
+
+namespace {
+
+template <typename T>
+static void set(const std::string& path, const T& value) {
+    std::ofstream file(path);
+    file << value;
+}
+
+} // anonymous namespace
+
+namespace vendor {
+namespace mokee {
+namespace biometrics {
+namespace fingerprint {
+namespace inscreen {
+namespace V1_0 {
+namespace implementation {
+
+FingerprintInscreen::FingerprintInscreen() {
+    xiaomiFingerprintService = IXiaomiFingerprint::getService();
+}
+
+Return<int32_t> FingerprintInscreen::getPositionX() {
+    return FOD_SENSOR_X;
+}
+
+Return<int32_t> FingerprintInscreen::getPositionY() {
+    return FOD_SENSOR_Y;
+}
+
+Return<int32_t> FingerprintInscreen::getSize() {
+    return FOD_SENSOR_SIZE;
+}
+
+Return<void> FingerprintInscreen::onStartEnroll() {
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onFinishEnroll() {
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onPress() {
+    set(DISPPARAM_PATH, DISPPARAM_FOD_BACKLIGHT_HBM);
+    xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_FOD);
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onRelease() {
+    set(DISPPARAM_PATH, DISPPARAM_FOD_BACKLIGHT_RESET);
+    xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_NONE);
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onShowFODView() {
+    set(FOD_STATUS_PATH, FOD_STATUS_ON);
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onHideFODView() {
+    set(FOD_STATUS_PATH, FOD_STATUS_OFF);
+    set(DISPPARAM_PATH, DISPPARAM_FOD_BACKLIGHT_RESET);
+    xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_NONE);
+    return Void();
+}
+
+Return<bool> FingerprintInscreen::handleAcquired(int32_t acquiredInfo, int32_t vendorCode) {
+    LOG(ERROR) << "acquiredInfo: " << acquiredInfo << ", vendorCode: " << vendorCode << "\n";
+    return false;
+}
+
+Return<bool> FingerprintInscreen::handleError(int32_t error, int32_t vendorCode) {
+    LOG(ERROR) << "error: " << error << ", vendorCode: " << vendorCode << "\n";
+    return false;
+}
+
+Return<void> FingerprintInscreen::setLongPressEnabled(bool) {
+    return Void();
+}
+
+Return<int32_t> FingerprintInscreen::getDimAmount(int32_t brightness) {
+    return 255 - brightness;
+}
+
+Return<bool> FingerprintInscreen::shouldBoostBrightness() {
+    return false;
+}
+
+Return<void> FingerprintInscreen::setCallback(const sp<::vendor::mokee::biometrics::fingerprint::inscreen::V1_0::IFingerprintInscreenCallback>& callback) {
+    (void) callback;
+    return Void();
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace inscreen
+}  // namespace fingerprint
+}  // namespace biometrics
+}  // namespace lineage
+}  // namespace vendor

TheAcanthite/device_xiaomi_grus

fod/FingerprintInscreen.cpp
@@ -1,0 +1,139 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "FingerprintInscreenService"
+
+#include "FingerprintInscreen.h"
+
+#include <android-base/logging.h>
+#include <fstream>
+
+#define COMMAND_NIT 10
+#define PARAM_NIT_FOD 3
+#define PARAM_NIT_NONE 0
+
+#define DISPPARAM_PATH "/sys/class/drm/card0-DSI-1/disp_param"
+#define DISPPARAM_FOD_BACKLIGHT_HBM "0xD007FF"
+#define DISPPARAM_FOD_BACKLIGHT_RESET "0xD01000"
+
+#define FOD_STATUS_PATH "/sys/devices/virtual/touch/tp_dev/fod_status"
+#define FOD_STATUS_ON 1
+#define FOD_STATUS_OFF 0
+
+#define FOD_SENSOR_X 455
+#define FOD_SENSOR_Y 1920
+#define FOD_SENSOR_SIZE 173
+
+namespace {
+
+template <typename T>
+static void set(const std::string& path, const T& value) {
+    std::ofstream file(path);
+    file << value;
+}
+
+} // anonymous namespace
+
+namespace vendor {
+namespace mokee {
+namespace biometrics {
+namespace fingerprint {
+namespace inscreen {
+namespace V1_0 {
+namespace implementation {
+
+FingerprintInscreen::FingerprintInscreen() {
+    xiaomiFingerprintService = IXiaomiFingerprint::getService();
+}
+
+Return<int32_t> FingerprintInscreen::getPositionX() {
+    return FOD_SENSOR_X;
+}
+
+Return<int32_t> FingerprintInscreen::getPositionY() {
+    return FOD_SENSOR_Y;
+}
+
+Return<int32_t> FingerprintInscreen::getSize() {
+    return FOD_SENSOR_SIZE;
+}
+
+Return<void> FingerprintInscreen::onStartEnroll() {
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onFinishEnroll() {
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onPress() {
+    set(DISPPARAM_PATH, DISPPARAM_FOD_BACKLIGHT_HBM);
+    xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_FOD);
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onRelease() {
+    set(DISPPARAM_PATH, DISPPARAM_FOD_BACKLIGHT_RESET);
+    xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_NONE);
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onShowFODView() {
+    set(FOD_STATUS_PATH, FOD_STATUS_ON);
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onHideFODView() {
+    set(FOD_STATUS_PATH, FOD_STATUS_OFF);
+    set(DISPPARAM_PATH, DISPPARAM_FOD_BACKLIGHT_RESET);
+    xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_NONE);
+    return Void();
+}
+
+Return<bool> FingerprintInscreen::handleAcquired(int32_t acquiredInfo, int32_t vendorCode) {
+    LOG(ERROR) << "acquiredInfo: " << acquiredInfo << ", vendorCode: " << vendorCode << "\n";
+    return false;
+}
+
+Return<bool> FingerprintInscreen::handleError(int32_t error, int32_t vendorCode) {
+    LOG(ERROR) << "error: " << error << ", vendorCode: " << vendorCode << "\n";
+    return false;
+}
+
+Return<void> FingerprintInscreen::setLongPressEnabled(bool) {
+    return Void();
+}
+
+Return<int32_t> FingerprintInscreen::getDimAmount(int32_t brightness) {
+    return 255 - brightness;
+}
+
+Return<bool> FingerprintInscreen::shouldBoostBrightness() {
+    return false;
+}
+
+Return<void> FingerprintInscreen::setCallback(const sp<::vendor::mokee::biometrics::fingerprint::inscreen::V1_0::IFingerprintInscreenCallback>& callback) {
+    (void) callback;
+    return Void();
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace inscreen
+}  // namespace fingerprint
+}  // namespace biometrics
+}  // namespace mokee
+}  // namespace vendor